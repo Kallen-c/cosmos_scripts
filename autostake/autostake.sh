fde2cb2648bda3c4d6cf1375126f580ed4749067
Aleksandr <90583810+Aleksandr7793@users.noreply.github.com>
2022-02-21 20:28:57 +0300

    Update autostake.sh

--- autostake/autostake.sh
+++ autostake/autostake.sh
@@ -5,8 +5,8 @@ WITHOU_COLOR='\033[0m'
 wallet=""  # name of wallet
 service="" # example cohod
 keyring="" # keyring flag, example --keyring-backend test
-DEL_ADDR=$(${service} keys show $wallet -a --bech acc ${keyring})
-VAL_ADDR=$(${service} keys show $wallet -a --bech val ${keyring})
+ADDR=$(${service} keys show $wallet -a --bech acc ${keyring})
+VAL=$(${service} keys show $wallet -a --bech val ${keyring})
 PWD='PWD' # password
 DELAY=300 # pause in sec
 chain="" # example darkmatter-1
@@ -14,13 +14,13 @@ denom="" # example ucoho
 gasfees="" # gas, fees flags example --gas auto --fees 200${denom}
 for (( ;; )); do
         echo -e "Get reward from Delegation"
-        echo -e "${PWD}\ny\n" | ${service} tx distribution withdraw-rewards ${VAL_ADDR} --chain-id ${chain} --from $wallet  --commission ${keyring} ${gasfees} -y
+        echo -e "${PWD}\ny\n" | ${service} tx distribution withdraw-rewards ${VAL} --chain-id ${chain} --from $wallet  --commission ${keyring} ${gasfees} -y
 for (( timer=20; timer>0; timer-- ))
         do
                 printf "* sleep for ${RED_COLOR}%02d${WITHOUT_COLOR} sec\r" $timer
                 sleep 1
         done
-BAL=$(${service} q bank balances ${DEL_ADDR} -o json | jq -r '.balances | .[].amount')
+BAL=$(${service} q bank balances ${ADDR} -o json | jq -r '.balances | .[].amount')
 echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} ${DENOM}\n"
         echo -e "Claim rewards\n"
         echo -e "${PWD}\n${PWD}\n" | ${service} tx distribution withdraw-all-rewards --from $wallet --chain-id ${chain} ${gasfees} ${keyring} -y
@@ -29,12 +29,12 @@ for (( timer=20; timer>0; timer-- ))
                 printf "* sleep for ${RED_COLOR}%02d${WITHOU_COLOR} sec\r" $timer
                 sleep 1
         done
-BAL=$(${service} q bank balances ${DEL_ADDR} -o json | jq -r '.balances | .[].amount');
+BAL=$(${service} q bank balances ${ADDR} -o json | jq -r '.balances | .[].amount');
         BAL=$(($BAL - 1000))
 echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} ${denom}\n"
         echo -e "Stake ALL\n"
 if (( BAL > 1000 )); then
-            echo -e "${PWD}\n${PWD}\n" | ${service} tx staking delegate ${VAL_ADDR} ${BAL}${denom} --from $wallet --chain-id ${chain} ${keyring} ${gasfees} -y
+            echo -e "${PWD}\n${PWD}\n" | ${service} tx staking delegate ${VAL} ${BAL}${denom} --from $wallet --chain-id ${chain} ${keyring} ${gasfees} -y
         else
                                 echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} ${denom} BAL < 1000 ((((\n"
         fi
